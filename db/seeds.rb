09c7f7ae70232711da7c11e4e851d781fd26e5b2
Yoko Harada <yokolet@gmail.com>
2023-06-12 19:58:09 +0900

    creates seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+users = FactoryBot.create_list(:user, 3)
+users.each_with_index do |user, u_idx|
+  posts = FactoryBot.create_list(:post, u_idx + 1, user_id: user.id)
+  posts.each_with_index do |post, p_idx|
+    FactoryBot.create(:comment, user_id: users[(u_idx + p_idx) % 3].id, post_id: post.id)
+  end
+end
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :comment do
-    
+    body { Faker::Lorem.question }
   end
 end
