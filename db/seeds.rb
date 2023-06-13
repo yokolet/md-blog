30769b1e45f0826ac2ded19f152d344ed3caa906
Yoko Harada <yokolet@gmail.com>
2023-06-13 21:39:47 +0900

    updates seeds.rb to have more various patterns

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+FactoryBot.create(:user)
 users = FactoryBot.create_list(:user, 3)
 users.each_with_index do |user, u_idx|
   posts = FactoryBot.create_list(:post, u_idx + 1, user_id: user.id)
   posts.each_with_index do |post, p_idx|
-    FactoryBot.create(:comment, user_id: users[(u_idx + p_idx) % 3].id, post_id: post.id)
+    (0...p_idx).each do |i|
+      FactoryBot.create(:comment, user_id: users[(u_idx + p_idx) % 3].id, post_id: post.id)
+    end
   end
 end
