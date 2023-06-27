e177efb027e3d0b9e38a48c8b537f455c02b49a7
Yoko Harada <yokolet@gmail.com>
2023-06-27 15:15:21 +0900

    updates a logic to generate code verifier

--- app/helpers/oauth2_config_helper.rb
+++ app/helpers/oauth2_config_helper.rb
@@ -1,8 +1,10 @@
 module Oauth2ConfigHelper
-  def getStateAndCodeVerifier
+  def getStateAndCodeVerifier(verifier_len=48)
+    verifier_len = verifier_len < 43 ? 43 : verifier_len
+    verifier_len = verifier_len > 128 ? 128 : verifier_len
     {
-      state: SecureRandom.base64(64),
-      code_verifier: SecureRandom.base64(32)
+      state: SecureRandom.urlsafe_base64((64 * 3) / 4, false),
+      code_verifier: SecureRandom.urlsafe_base64((verifier_len * 3) / 4, false)
     }
   end
 end
--- spec/factories/pkces.rb
+++ spec/factories/pkces.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :pkce do
-    state { SecureRandom.base64(64) }
-    code_verifier { SecureRandom.base64(32) }
+    state { SecureRandom.urlsafe_base64((64 * 3) / 4, false) }
+    code_verifier { SecureRandom.urlsafe_base64((64* 3) / 4, false) }
   end
 end
