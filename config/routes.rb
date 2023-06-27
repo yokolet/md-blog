8b510a82fa474dee28da6f4227bb028ddb096dae
Yoko Harada <yokolet@gmail.com>
2023-06-27 23:14:43 +0900

    adds a controller for root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get 'oauth2/twitter'
   get 'oauth2_config/twitter'
-  get 'oauth/twitter'
   post "/graphql", to: "graphql#execute"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
